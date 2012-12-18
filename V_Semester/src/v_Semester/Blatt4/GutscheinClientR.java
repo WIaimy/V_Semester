1fc0e554cee3df9bb282bf20149b16de12bd2b14
WIaimy <github@joebges.com>
2012-12-18 11:11:48 +0100

    Läuft

--- V_Semester/src/v_Semester/Blatt4/GutscheinClientR.java
+++ V_Semester/src/v_Semester/Blatt4/GutscheinClientR.java
@@ -8,21 +8,28 @@ import v_Semester.Blatt0.Gutschein;
 
 public class GutscheinClientR {
 
-	public static void main(String[] para){
+	public static void main(String[] para) {
 		Scanner sc = new Scanner(System.in);
-		try{
-			GutscheinInterface gi = (GutscheinInterface) Naming.lookup("rmi://127.0.0.1:1221/Gutschein");
-			System.out.println("Kundennummer:\t");
-			int knr = sc.nextInt();
-			//Liste holen
-			List<Gutschein> l = gi.getGutscheine(knr);
-			//Ausgeben
-			for(Gutschein g : l)
-				System.out.println(g);
+
+		try {
+			GutscheinInterface gi = (GutscheinInterface) Naming
+					.lookup("rmi://localhost:1099/Gutschein");
+			while (true) {
+				System.out.println("Kundennummer:\t");
+				int knr = sc.nextInt();
+				if (knr == -1)
+					return;
 		
-		}catch(Exception e){
+				// Liste holen
+				List<Gutschein> l = gi.getGutscheine(knr);
+				
+				// Ausgeben
+				for (Gutschein g : l)
+					System.out.println(g);
+			}
+		} catch (Exception e) {
 			e.printStackTrace();
-		}finally{
+		} finally {
 			sc.close();
 		}
 	}
--- V_Semester/src/v_Semester/Blatt4/GutscheinServerR.java
+++ V_Semester/src/v_Semester/Blatt4/GutscheinServerR.java
@@ -1,13 +1,17 @@
 package v_Semester.Blatt4;
 
 import java.rmi.Naming;
+import java.rmi.registry.LocateRegistry;
 
 public class GutscheinServerR {
 
 	public static void main(String[] para){
+		
 		try{
+			LocateRegistry.createRegistry(1099);
+			
 			GutscheinObjekt go = new GutscheinObjekt();
-			Naming.rebind("rmi://127.0.0.1:1221/Gutschein", go);
+			Naming.rebind("rmi://localhost:1099/Gutschein", go);
 			System.out.println("Server läuft");
 		}catch(Exception e){
 			e.printStackTrace();
